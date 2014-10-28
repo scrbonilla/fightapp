a51671ac6542c0422ac2acbc82f4e4b84e53ebf0
scrbonilla <scrbonilla84@gmail.com>
2014-10-28 01:35:34 -0400

    Migration for video info; created many files and updated many others
    Parsing through API data, using iframe, javascript and coffee.  This is a test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-require 'bootstrap'
+//= require bootstrap
+//= require jquery.bind_with_delay
+//= require yt_player
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141028042738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "videos", force: true do |t|
+    t.string   "link"
+    t.string   "title"
+    t.string   "author"
+    t.string   "duration"
+    t.integer  "likes"
+    t.integer  "dislikes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
